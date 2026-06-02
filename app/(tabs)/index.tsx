bf8c3c3baf0a95a9b7f4538e8c4a57a8bfc79983
Sahil <sahilaktar648@gmail.com>
2026-06-02 13:10:22 +0530

    font setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,31 +2,32 @@ import "@/global.css";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-import { styled } from 'nativewind'
+import { styled } from "nativewind";
 
-
-const SafeAreaView = styled(RNSafeAreaView)
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">Hello S@nchit@</Text>
+      <Text className="text-xl font-sans- font-sans-boldbold text-success">
+        Hello S@nchit@
+      </Text>
       <Link
         href="/home"
-        className="mt-4 px-4 py-2 bg-success text-white rounded"
+        className="mt-4 font-sans-bold px-4 py-2 bg-primary text-white rounded"
       >
         Go to Home
       </Link>
-      <View >
+      <View>
         <Link
           href="/(auth)/sign-in"
-          className="mt-4 px-4 py-2 bg-success text-white rounded"
+          className="mt-4 font-sans-bold px-4 py-2 bg-primary text-white rounded"
         >
           SignIn
         </Link>
         <Link
           href="/(auth)/sign-up"
-          className="mt-4 px-4 py-2 bg-success text-white rounded"
+          className="mt-4 font-sans-bold px-4 py-2 bg-primary text-white rounded"
         >
           SignUp
         </Link>
